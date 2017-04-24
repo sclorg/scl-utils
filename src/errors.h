b5e6bf7718b269fa250acbf03713b2ad2417cff7
Ville Skyttä <ville.skytta@iki.fi>
2017-04-24 09:30:19 +0300

    Spelling fixes

--- src/errors.h
+++ src/errors.h
@@ -8,7 +8,7 @@ typedef enum {
 	EDISK,   /* some disk operation failed */
 	ECONFIG, /* some problem with scl configuration */
     ERUN,    /* some problem with running external program */
-    ERPMLIB, /* some occured in rpm library */
+    ERPMLIB, /* some occurred in rpm library */
     ESYS,    /* unexpected system error, e. g. unexpected fail of function from stdlib */
 } scl_rc;
 
